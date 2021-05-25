bf090059c77663bca7298e84b104e8b315b7c72d
aliptonga <aliptonga@gmail.com>
2021-05-25 16:26:22 -0700

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class BranchingPractice {
+public class BranchingPractice {
+
+    public static void main(String[] args) {
+        //created new project
+        //enabled version control
+        //added .gitignore with .idea , target folder and .iml file
+        //made initial commit by keeping track of all files
+        //shared project on github
+    }
 }
